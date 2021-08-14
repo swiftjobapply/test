149e0d90aa48de7bdbf500f392f14880f2f674e5
swiftjobapply <88750232+swiftjobapply@users.noreply.github.com>
2021-08-14 23:40:16 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,15 +1,17 @@
-var mysql=require('mysql')
+var mysql = require('mysql')
 
-var db=mysql.createConnection({
-    host:'127.0.0.1',
-    user:'root',
-    database:'swiftjobs'
+var db = mysql.createConnection({
+    host: 'mysql-5-8f08.hostman.site',
+    user: 'hostman',
+    port: "3313",
+    password: "af9e4237",
+    database: 'database'
 })
-db.connect(err=>{
-	if(err){
-		 console.log('not connected')
-	}else{
-          console.log('connected')
-	}
+db.connect(err => {
+    if (err) {
+        console.log('not connected')
+    } else {
+        console.log('connected')
+    }
 })
-module.exports=db
+module.exports = db
