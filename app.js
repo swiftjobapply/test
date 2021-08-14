6685be6c4dcd72398c00edd9a3bc7169e33c1994
swiftjobapply <88750232+swiftjobapply@users.noreply.github.com>
2021-08-14 02:32:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express')
-var path = require('path')
 var fileUpload = require('express-fileupload');
-var bodyParser = require('body-parser')
 var config = require('./config/config')
 var routes = require('./router/router')
 var app = express()
 app.use(fileUpload());
-app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 app.use('/swift', routes)
-app.use(express.static(path.join(__dirname, "frontend")))
-var port = process.env.PORT || 2000
+var port = 8000
 app.listen(port)
+app.get('/',(req,res)=>{
+res.send('choke ooo')
+})
+app.get('/mofu',(req,res)=>{
+res.send('mofu')
+})
