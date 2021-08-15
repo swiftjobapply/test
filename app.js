8cbbf2be2c46d556793be40c094d64b2d223c5ee
swiftjobapply <88750232+swiftjobapply@users.noreply.github.com>
2021-08-15 01:11:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var fileUpload = require('express-fileupload');
 var config = require('./config/config')
-var dbConfig = require('./config/dbConfig')
+//var dbConfig = require('./config/dbConfig')
 var routes = require('./router/router')
 var path=require('path')
 var app = express()
@@ -11,8 +11,8 @@ app.use('/swift', routes)
 var port = process.env.PORT || 5000
 app.listen(port)
 
-app.get('/startSql',(req,res)=>{
-  dbConfig()
-  console.log('start')
-res.send('creating')
-})
+///app.get('/startSql',(req,res)=>{
+//  dbConfig()
+//  console.log('start')
+//res.send('creating')
+//})
